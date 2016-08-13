58291853afaf37bed284fee47b3a7cc0f99aca4d
shmp0722 <shmp0722@gmail.com>
2016-08-13 18:00:08 +0900

    axis off is easy. Added event source

--- EventAnalysis.m
+++ EventAnalysis.m
@@ -1,4 +1,4 @@
-function TrendAnalysis
+function EventAnalysis
 
 % just rendering a scatterplot showing visual fiedl loss progression. 
 %
@@ -7,15 +7,18 @@ function TrendAnalysis
 %% 
 
 x = 1:7;
-X = [28,27,27,26,25,24,23];
+X = [28,27,27,26,26,26,20];
 y = X-randn(1,7)*0.7;
 
 figure;
 hold on;
 scatter(x,y,100,'filled')
 set(gca,'ylim',[20,30])
-lsline
+
+% percentile = prctile(y,5);
+% refline([0,percentile])
 axis off
+
 
 hold off
 end
